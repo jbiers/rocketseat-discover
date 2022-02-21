c434ae6801430712190f4660d214716299572356
jbiers <juliabiersuriano@gmail.com>
2022-02-21 11:53:07 -0400

    adapting to modular js

--- javascript-patterns/modules/peopleAdder/script.js
+++ javascript-patterns/modules/peopleAdder/script.js
@@ -1,8 +1,10 @@
+/*
 let peopleArray = ['Mike', 'Eleven', 'Jake'];
 
 const people = document.querySelector('.people');
 const addBtn = document.querySelector('.adder-button');
 const addInpt = document.querySelector('.adder-input');
+const template = document.querySelector('.template');
 
 function addPerson() {
     let name = addInpt.value;
@@ -50,3 +52,75 @@ function renderPeople() {
 addBtn.addEventListener('click', addPerson);
 
 renderPeople();
+
+*/
+
+// OBJECT LITERAL PATTERN
+
+(function () {
+    let people = {
+        peopleArray: ['Jake'],
+
+        init: function () {
+            this.cacheDom();
+            this.bindEvents();
+            this.render();
+        },
+
+        cacheDom: function () {
+            // Only one document search!!
+            this.module = document.querySelector('.adder-container');
+            this.people = this.module.querySelector('.people');
+            this.addBtn = this.module.querySelector('.adder-button');
+            this.addInpt = this.module.querySelector('.adder-input');
+        },
+
+        render: function () {
+            while (this.people.firstChild) {
+                this.people.removeChild(this.people.firstChild);
+            }
+
+            for (let i = 0; i < this.peopleArray.length; i++) {
+                this.person = this.createPerson(this.peopleArray[i]);
+                this.person.addEventListener('click', () => {
+                    this.deletePerson(this.peopleArray[i]);
+                });
+
+                this.people.appendChild(this.person);
+            }
+        },
+
+        bindEvents: function () {
+            this.addBtn.addEventListener('click', this.addPerson);
+        },
+
+        addPerson: function (name) {
+            this.name = this.addInpt.value;
+            this.addInpt.value = '';
+
+            this.peopleArray.push(name);
+
+            this.render();
+        },
+
+        deletePerson: function (name) {
+            this.index = this.peopleArray.indexOf(name);
+
+            this.peopleArray.splice(this.index, 1);
+
+            this.render();
+        },
+
+        createPerson: function (name) {
+            this.personDiv = document.createElement('div');
+            this.personDiv.classList.add('person', 'card');
+
+            this.personDiv.innerHTML = `<span class="name">${name}</span>
+            <div class="delete">x</div>`;
+
+            return this.personDiv;
+        }
+    }
+
+    people.init();
+})();
