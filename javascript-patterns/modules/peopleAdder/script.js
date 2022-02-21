dad9c77b50c7f7dd74824b2a99f1649a8d7e4e4f
jbiers <juliabiersuriano@gmail.com>
2022-02-21 10:03:48 -0400

    finished spaghetti code

--- javascript-patterns/modules/peopleAdder/script.js
+++ javascript-patterns/modules/peopleAdder/script.js
@@ -1,4 +1,4 @@
-let peopleArray = ['Mike', 'Eleven'];
+let peopleArray = ['Mike', 'Eleven', 'Jake'];
 
 const people = document.querySelector('.people');
 const addBtn = document.querySelector('.adder-button');
@@ -6,21 +6,47 @@ const addInpt = document.querySelector('.adder-input');
 
 function addPerson() {
     let name = addInpt.value;
+    addInpt.value = '';
+
     peopleArray.push(name);
 
     renderPeople();
 }
 
 function deletePerson(name) {
-    peopleArray.filter(person => person != name);
+    let index;
+
+    index = peopleArray.indexOf(name);
+    peopleArray.splice(index, 1);
 
     renderPeople();
 }
 
-function renderPeople() {
-    console.log(people);
+function createPerson(name) {
+    let personDiv = document.createElement('div');
+    personDiv.classList.add('person', 'card');
+
+    personDiv.innerHTML = `<span class="name">${name}</span>
+    <div class="delete">x</div>`;
+
+    return personDiv;
 }
 
-addBtn.addEventListener('click', addPerson());
+function renderPeople() {
+    while (people.firstChild) {
+        people.removeChild(people.firstChild);
+    }
+
+    for (let i = 0; i < peopleArray.length; i++) {
+        let person = createPerson(peopleArray[i]);
+        person.addEventListener('click', () => {
+            deletePerson(peopleArray[i]);
+        });
+
+        people.appendChild(person);
+    }
+}
+
+addBtn.addEventListener('click', addPerson);
 
 renderPeople();
